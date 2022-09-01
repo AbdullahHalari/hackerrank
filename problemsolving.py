ceb54672b430bb9acb7bb8eb090e265c7304b56b
Abdullah Muhammad Ibrahim <56643834+AbdullahHalari@users.noreply.github.com>
2022-09-02 03:11:58 +0500

    add 3

--- problemsolving.py
+++ problemsolving.py
@@ -38,4 +38,40 @@ for i in ar:
 print(sum)
      
 #3
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+a = list(map(int, input().rstrip().split()))
+
+b = list(map(int, input().rstrip().split()))
+# a = [17,28,30]
+# b = [99,16,8]
+i = 0
+alice = 0
+bob = 0
+while i < 3:
+    if(a[i]>b[i]):
+        alice+=1
+    elif(a[i]<b[i]):
+        bob+=1
+    elif(a[i]==b[i]):
+        print()
+        
+    i+=1
+print(alice)
+print(bob)
+    
 
