20974265864b99967d6a9e3e508e46a2ed434c49
Abdullah Muhammad Ibrahim <56643834+AbdullahHalari@users.noreply.github.com>
2022-09-02 02:54:26 +0500

    first init

---
+++ problemsolving.py
@@ -0,0 +1,41 @@
+#1
+
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+   return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
+
+#2
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+
+    # Write your code here
+
+ar_count = int(input().strip())
+ar = list(map(int, input().rstrip().split()))
+sum =0
+for i in ar:
+    sum+=i
+print(sum)
+     
+#3
+
