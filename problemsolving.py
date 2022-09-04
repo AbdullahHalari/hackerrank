04a864c4f747655e99db8a9f604d1da8c1222bcf
Abdullah Muhammad Ibrahim <56643834+AbdullahHalari@users.noreply.github.com>
2022-09-05 02:37:48 +0500

    add 6

--- problemsolving.py
+++ problemsolving.py
@@ -123,6 +123,51 @@ while(count<n):
     j-=1
 
 print(abs(sum-add))    
+
+#6
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+# def plusMinus(arr):
+    # Write your code here
+
+# if __name__ == '__main__':
+n = int(input().strip())
+
+arr = list(map(int, input().rstrip().split()))
+i=0
+positive=0
+negative=0
+zero=0
+
+while i < n:
+    
+    if(arr[i]>0):
+        positive+=1
+    elif(arr[i]<0):
+        negative+=1
+    else:
+        zero+=1
+        
+    i+=1
+print("{:.6f}".format(positive/n))
+print("{:.6f}".format(negative/n))
+print("{:.6f}".format(zero/n))
+# plusMinus(arr)
+
+
+
     
 
 
